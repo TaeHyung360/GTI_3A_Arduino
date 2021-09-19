7e1a776d1ebc86dd97bc724fd2a5f48418886106
Juan <tostyfis360@gmail.com>
2021-09-19 10:58:40 +0200

    Introducción de código de Arduino.
    Se han introducido diseños y comentarios.

---
+++ src/main/HolaMundoIBeacon/Publicador.h
@@ -0,0 +1,140 @@
+// -*- mode: c++ -*-
+
+// --------------------------------------------------------------
+// Jordi Bataller i Mascarell
+// --------------------------------------------------------------
+
+#ifndef PUBLICADOR_H_INCLUIDO
+#define PUBLICADOR_H_INCLUIDO
+
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+class Publicador {
+
+  // ............................................................
+  // ............................................................
+private:
+
+  uint8_t beaconUUID[16] = { 
+	'E', 'P', 'S', 'G', '-', 'G', 'T', 'I', 
+	'-', 'P', 'R', 'O', 'Y', '-', '3', 'A'
+	};
+
+  // ............................................................
+  // ............................................................
+public:
+  EmisoraBLE laEmisora {
+	"GTI-3A", //  nombre emisora
+	  0x004c, // fabricanteID (Apple)
+	  4 // txPower
+	  };
+  
+  const int RSSI = -53; // por poner algo, de momento no lo uso
+
+  // ............................................................
+  // ............................................................
+public:
+
+  // ............................................................
+  // ............................................................
+  enum MedicionesID  {
+	CO2 = 11,
+	TEMPERATURA = 12,
+	RUIDO = 13
+  };
+
+  // ............................................................
+  // ............................................................
+   /**
+   * Cosntructor de publicador
+   * publicador()
+   * 
+   */
+  Publicador( ) {
+	// ATENCION: no hacerlo aquí. (*this).laEmisora.encenderEmisora();
+	// Pondremos un método para llamarlo desde el setup() más tarde
+  } // ()
+
+  // ............................................................
+  // ............................................................
+   /**
+   * encenderEmisora()
+   * 
+   */
+  void encenderEmisora() {
+	(*this).laEmisora.encenderEmisora();
+  } // ()
+
+  // ............................................................
+  // ............................................................
+   /**
+   * valorCO2:N, contador:N, tiempoEspera:N -> publicarCO2()
+   * 
+   * @param valorCO2 tomado por el sensor
+   * @param contador 
+   * @param tiempoEspera 
+   */
+  void publicarCO2( int16_t valorCO2, uint8_t contador,
+					long tiempoEspera ) {
+
+	//
+	// 1. empezamos anuncio
+	//
+	uint16_t major = (MedicionesID::CO2 << 8) + contador;
+	(*this).laEmisora.emitirAnuncioIBeacon( (*this).beaconUUID, 
+											major,
+											valorCO2, // minor
+											(*this).RSSI // rssi
+									);
+
+	/*
+	Globales::elPuerto.escribir( "   publicarCO2(): valor=" );
+	Globales::elPuerto.escribir( valorCO2 );
+	Globales::elPuerto.escribir( "   contador=" );
+	Globales::elPuerto.escribir( contador );
+	Globales::elPuerto.escribir( "   todo="  );
+	Globales::elPuerto.escribir( major );
+	Globales::elPuerto.escribir( "\n" );
+	*/
+
+	//
+	// 2. esperamos el tiempo que nos digan
+	//
+	esperar( tiempoEspera );
+
+	//
+	// 3. paramos anuncio
+	//
+	(*this).laEmisora.detenerAnuncio();
+  } // ()
+
+  // ............................................................
+  // ............................................................
+  /**
+   * valorTemperatura:N, contador:N, tiempoEspera:N -> publicarTemperatura()
+   * 
+   * @param valorTemperatura tomado por el sensor
+   * @param contador 
+   * @param tiempoEspera 
+   */
+  void publicarTemperatura( int16_t valorTemperatura,
+							uint8_t contador, long tiempoEspera ) {
+
+	uint16_t major = (MedicionesID::TEMPERATURA << 8) + contador;
+	(*this).laEmisora.emitirAnuncioIBeacon( (*this).beaconUUID, 
+											major,
+											valorTemperatura, // minor
+											(*this).RSSI // rssi
+									);
+	esperar( tiempoEspera );
+
+	(*this).laEmisora.detenerAnuncio();
+  } // ()
+	
+}; // class
+
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+#endif
